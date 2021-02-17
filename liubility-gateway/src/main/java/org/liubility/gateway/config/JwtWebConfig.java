5c5ebe4f87a5a29e99f68a1e935ea221ffd46c39
jdragon <1061917196@qq.com>
2021-02-17 14:41:56 +0800

    init...

--- liubility-gateway/src/main/java/org/liubility/gateway/config/JwtWebConfig.java
+++ liubility-gateway/src/main/java/org/liubility/gateway/config/JwtWebConfig.java
@@ -1,6 +1,8 @@
 package org.liubility.gateway.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.liubility.api.AccountServiceProvider;
+import org.liubility.commons.jwt.JwtProperty;
 import org.liubility.commons.jwt.JwtServiceImpl;
 import org.liubility.commons.dto.account.AccountDto;
 import org.liubility.commons.http.response.normal.Result;
@@ -11,10 +13,13 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.util.AntPathMatcher;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilter;
 import org.springframework.web.server.WebFilterChain;
 import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 /**
  * @Author: Jdragon
@@ -23,47 +28,56 @@ import reactor.core.publisher.Mono;
  * @Description:
  */
 @Configuration
+@Slf4j
 public class JwtWebConfig implements WebFilter {
+
+    @Autowired
+    private JwtProperty jwtProperty;
 
     @Autowired
     private JwtServiceImpl jwtService;
 
-    @Autowired
-    private AccountServiceProvider accountServiceProvider;
-
     @Override
     public Mono<Void> filter(ServerWebExchange serverWebExchange, WebFilterChain webFilterChain) {
         ServerHttpRequest request = serverWebExchange.getRequest();
-        if (request.getPath().value().contains("/account/login")) {
+
+        AntPathMatcher antPathMatcher = new AntPathMatcher();
+        String path = request.getPath().value();
+        boolean valid = jwtProperty.getIgnore().stream().anyMatch(item -> antPathMatcher.match(item, path));
+        if (valid) {
             return webFilterChain.filter(serverWebExchange);
         }
 
         ServerHttpResponse response = serverWebExchange.getResponse();
         String authorization = request.getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
         if (authorization == null || !authorization.startsWith("Bearer ")) {
-            return this.setErrorResponse(response, "未携带token");
+            return this.setErrorResponse(response, Result.authFail("未携带token"));
         }
+
         String token = authorization.substring(7);
-        if(!jwtService.isTokenExpired(token)){
-            return this.setErrorResponse(response,"token已过期");
+        try {
+            if (jwtService.isTokenExpired(token)) {
+                return this.setErrorResponse(response, Result.authFail("token无效"));
+            }
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            e.printStackTrace();
+            return this.setErrorResponse(response, Result.error("token解析异常"));
         }
+
         try {
             String username = jwtService.getUsernameFromToken(token);
-            AccountDto loginAccountByName = accountServiceProvider.getLoginAccountByName(username);
-            if(loginAccountByName == null){
-                return this.setErrorResponse(response,"token异常");
-            }
-            System.out.println(loginAccountByName);
+            ServerHttpRequest mutateReq = request.mutate().header("username", username).build();
+            return webFilterChain.filter(serverWebExchange.mutate().request(mutateReq).build());
         } catch (Exception e) {
-            return this.setErrorResponse(response, Result.error(e.getMessage()));
+            log.error(e.getMessage());
+            e.printStackTrace();
+            return this.setErrorResponse(response, Result.unKnowError("系统繁忙"));
         }
-        return webFilterChain.filter(serverWebExchange);
-
     }
 
     protected Mono<Void> setErrorResponse(ServerHttpResponse response, Object object) {
         response.getHeaders().setContentType(MediaType.APPLICATION_JSON);
         return response.writeWith(Mono.just(response.bufferFactory().wrap(JsonUtils.object2Byte(object))));
-
     }
 }
